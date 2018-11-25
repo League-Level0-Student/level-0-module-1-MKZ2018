7e3aaeecef11cdd02497fa1e2ed3b865a0df3b1d
MKZ2018 <league@worthymini-6.attlocal.net>
2018-11-25 11:31:21 -0800

    module 1

---
+++ src/ifs_ints_and_loops/Unbirthday.java
@@ -0,0 +1,18 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+	String birthday;
+	birthday = JOptionPane.showInputDialog("When is your birthday? (month, day)");
+	if (birthday.equals("11/25") {
+		JOptionPane.showMessageDialog(null, "Happy Birthday!");
+	}
+	else {
+	JOptionPane.showMessageDialog(null, "Happy, unbirthday!");
+		
+	}
+}
+
+
